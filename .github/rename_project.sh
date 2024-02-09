8f1e9710f866c8cd0a6f7fbbfeb5676f8f2d4d2c
ketabi <ketabi@users.noreply.github.com>
2024-02-09 10:59:47 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ketabi"
+original_name="vst_api"
+original_urlname="vst-api"
+original_description="Awesome vst_api created by ketabi"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv vst_api $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- vst_api/app.py
+++ vst_api/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+vst_api API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="vst_api",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://vst_api.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "ketabi",
+        "url": "http://vst_api.com/contact/",
+        "email": "ketabi@vst_api.com",
     },
     license_info={
         "name": "The Unlicense",
